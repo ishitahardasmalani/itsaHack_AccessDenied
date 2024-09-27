c92133f41504a28e810643aa74dde84dd1a11e08
Mihika <mihikakaprani@gmail.com>
2024-09-27 22:06:16 +0530

    commit

---
+++ app/screens/MutualFunds.jsx
@@ -0,0 +1,16 @@
+import { StyleSheet, Text, View } from 'react-native'
+import React from 'react'
+
+const MutualFunds = () => {
+  return (
+    <View>
+      <Text>MutualFunds</Text>
+    </View>
+  )
+}
+
+export default MutualFunds
+
+const styles = StyleSheet.create({
+    
+})
