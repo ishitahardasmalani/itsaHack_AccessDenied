5370a021e6d4f3e4504415b1f58f55f838da16db
Ishita Hardasmalani <143803109+ishitahardasmalani@users.noreply.github.com>
2024-09-28 17:59:54 +0530

    commit

---
+++ app/components/Quizdata.jsx
@@ -0,0 +1,238 @@
+const quizData = [
+  {
+    question: "What is the definition of 'Compound Interest'?",
+    options: [
+      "The interest earned only on the initial principal amount",
+      "The interest earned on both the initial principal and the accumulated interest",
+      "The interest earned after a fixed period of time",
+      "The interest earned on loans only",
+    ],
+    correctAnswer: 1,
+    img: require("../assets/tax_4334638.png"),
+  },
+  {
+    question: "What is a 'Credit Score' used for?",
+    options: [
+      "Determining your favorite color",
+      "Assessing your eligibility for a loan or credit",
+      "Measuring your shoe size",
+      "Calculating the distance to the moon",
+    ],
+    correctAnswer: 1,
+    img: require("../assets/tax_4334638.png"),
+  },
+  {
+    question: "What is 'Budgeting' in personal finance?",
+    options: [
+      "A type of financial investment",
+      "A method to increase credit score",
+      "A plan for managing income and expenses",
+      "A type of loan",
+    ],
+    correctAnswer: 2,
+    img: require("../assets/tax_4334638.png"),
+  },
+  {
+    question: "What is 'Diversification' in investment?",
+    options: [
+      "Putting all your money into a single investment",
+      "Spreading your investments across different assets",
+      "Hiding money under your mattress",
+      "Buying lottery tickets",
+    ],
+    correctAnswer: 2,
+    img: require("../assets/tax_4334638.png"),
+  },
+];
+
+const quizData1 = [
+  {
+    question: "What is the definition of 'Savings'?",
+    options: [
+      "Money kept in a piggy bank",
+      "Money you spend on toys",
+      "Money you keep aside for future needs",
+      "Money used for buying snacks",
+    ],
+    correctAnswer: 2,
+    img: require("../assets/tax_4334638.png"),
+  },
+  {
+    question: "What is a 'Piggy Bank' used for?",
+    options: [
+      "Holding your favorite snacks",
+      "Saving money for the future",
+      "A place to keep your toys",
+      "A type of game",
+    ],
+    correctAnswer: 1,
+    img: require("../assets/tax_4334638.png"),
+  },
+  {
+    question: "What does 'Budget' mean?",
+    options: [
+      "A plan for managing your money",
+      "A type of cookie",
+      "A game you play with friends",
+      "A way to exercise",
+    ],
+    correctAnswer: 0,
+    img: require("../assets/tax_4334638.png"),
+  },
+  {
+    question: "What is 'Income'?",
+    options: [
+      "Money you spend on toys",
+      "Money you earn or receive",
+      "Money used for buying clothes",
+      "Money saved for a vacation",
+    ],
+    correctAnswer: 1,
+    img: require("../assets/tax_4334638.png"),
+  },
+];
+
+const quizData2 = [
+  {
+    question: "What is a 'Credit Card'?",
+    options: [
+      "A card for playing card games",
+      "A card that allows you to borrow money to make purchases",
+      "A card used for board games",
+      "A card for collecting points at a store",
+    ],
+    correctAnswer: 1,
+    img: require("../assets/tax_4334638.png"),
+  },
+  {
+    question: "What does 'Loan' mean?",
+    options: [
+      "Borrowing something from a friend",
+      "Borrowing money from a bank with the promise to pay it back",
+      "A type of exercise",
+      "A type of pet",
+    ],
+    correctAnswer: 1,
+    img: require("../assets/tax_4334638.png"),
+  },
+  {
+    question: "What is a 'Savings Account' used for?",
+    options: [
+      "Saving your favorite snacks",
+      "Saving money for future needs or emergencies",
+      "A place to save your toys",
+      "A type of computer game",
+    ],
+    correctAnswer: 1,
+    img: require("../assets/tax_4334638.png"),
+  },
+  {
+    question: "What is 'Debt'?",
+    options: [
+      "A way to make new friends",
+      "Money that you owe to someone else",
+      "A type of dessert",
+      "A type of book",
+    ],
+    correctAnswer: 1,
+    img: require("../assets/tax_4334638.png"),
+  },
+];
+
+const quizData3 = [
+  {
+    question: "What is 'Taxes'?",
+    options: [
+      "A way to collect candy",
+      "Money paid to the government to fund public services",
+      "A type of toy",
+      "A type of movie",
+    ],
+    correctAnswer: 1,
+    img: require("../assets/tax_4334638.png"),
+  },
+  {
+    question: "What is 'Borrowing'?",
+    options: [
+      "Sharing your toys with a friend",
+      "Taking something from someone with the promise to give it back",
+      "A type of game",
+      "A type of fruit",
+    ],
+    correctAnswer: 1,
+    img: require("../assets/tax_4334638.png"),
+  },
+  {
+    question: "What is a 'Credit Score'?",
+    options: [
+      "A score for your performance in school",
+      "A score that measures your ability to save money",
+      "A score that assesses your eligibility for loans or credit",
+      "A score for playing video games",
+    ],
+    correctAnswer: 2,
+    img: require("../assets/tax_4334638.png"),
+  },
+  {
+    question: "What does 'Investment' mean?",
+    options: [
+      "Spending money on buying toys",
+      "Putting money into something with the hope of making more money",
+      "Buying a new video game",
+      "Saving money in a piggy bank",
+    ],
+    correctAnswer: 1,
+    img: require("../assets/tax_4334638.png"),
+  },
+];
+
+const quizData4 = [
+  {
+    question: "What is 'Compound Interest'?",
+    options: [
+      "The interest earned only on the initial principal amount",
+      "The interest earned on both the initial principal and the accumulated interest",
+      "The interest earned after a fixed period of time",
+      "The interest earned on loans only",
+    ],
+    correctAnswer: 1,
+    img: require("../assets/tax_4334638.png"),
+  },
+  {
+    question: "What is 'Inflation'?",
+    options: [
+      "A rise in the general level of prices for goods and services",
+      "A type of exercise",
+      "A type of ice cream flavor",
+      "A term used in cooking to mean 'adding spices'",
+    ],
+    correctAnswer: 0,
+    img: require("../assets/tax_4334638.png"),
+  },
+  {
+    question: "What is 'Budgeting' in personal finance?",
+    options: [
+      "A type of financial investment",
+      "A method to increase credit score",
+      "A plan for managing income and expenses",
+      "A type of loan",
+    ],
+    correctAnswer: 2,
+    img: require("../assets/tax_4334638.png"),
+  },
+  {
+    question: "What is 'Diversification' in investment?",
+    options: [
+      "Putting all your money into a single investment",
+      "Spreading your investments across different assets",
+      "Hiding money under your mattress",
+      "Buying lottery tickets",
+    ],
+    correctAnswer: 1,
+    img: require("../assets/tax_4334638.png"),
+  },
+];
+
+export { quizData1, quizData2, quizData3, quizData4 };
+
+export default quizData;
